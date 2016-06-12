f4e061265b30fa3ba9e90680b3d18fd42cf9ed37
Feram <feram@feram.co>
2016-06-12 21:02:55 +0000

    Fix typos

--- docs/swagger-ui.js
+++ docs/swagger-ui.js
@@ -8927,7 +8927,7 @@ function readBlockScalar(state, nodeIndent) {
         }
       }
 
-      // Break this `while` cycle and go to the funciton's epilogue.
+      // Break this `while` cycle and go to the function's epilogue.
       break;
     }
 
@@ -21961,7 +21961,7 @@ Promise.prototype.race = function () {
  * accepts the operation name, a resolver, and any further arguments that would
  * have been forwarded to the appropriate method above had a method been
  * provided with the proper name.  The API makes no guarantees about the nature
- * of the returned object, apart from that it is usable whereever promises are
+ * of the returned object, apart from that it is usable wherever promises are
  * bought and sold.
  */
 Q.makePromise = Promise;
